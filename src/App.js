99f9510d0f5d2d9eff7f837fbabda491c8561e63
divya-kkl <divyakkl2002@gmail.com>
2026-08-18 10:07:28 +0530

    small-changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HeroSection from './Componenet/HeroSection/HeroSection';
 import Category from './Componenet/Category/Category';
 import TopSpecialties from './Componenet/TopSpecialties/TopSpecialties';
 import FeaturedDoctors from './Componenet/FeaturedDoctors/FeaturedDoctors';
-import Scroll_banner from './Componenet/Scroll-banner/Scroll-banner';
+import ScrollBanner from './Componenet/Scroll-banner/Scroll-banner';
 import ReasonSection from './Componenet/Reason-Section/Reason-Section';
 
 function App() {
@@ -15,10 +15,10 @@ function App() {
       <Topheader />
       <Header />
       <HeroSection />
-       <Category />
-      <TopSpecialties /> 
+      <Category />
+      <TopSpecialties />
       <FeaturedDoctors />
-      < Scroll_banner />
+      <ScrollBanner />
       <ReasonSection />
     </div>
   );
